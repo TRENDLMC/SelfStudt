b56b18dfb8b95530859f9e5f62430dcb6595a0ee
LMC <trend7936@naver.com>
2023-04-15 20:32:04 +0900

    출력문

---
+++ int_exe/sout.java
@@ -0,0 +1,7 @@
+public class sout {
+    public static void main(String[] args) {
+        // 시스템 출력 명령어 "sout" System.out.println  이클립스의경우 일일입력해야함 처음s는 대문자로 꼭작성 아님오류남 System.out.println();
+        // 출력시 단어를 출력하고싶다면 ""큰 따움표 사이에 단어와 숫자를 입력해야함
+        System.out.println("hello java");
+    }
+}
